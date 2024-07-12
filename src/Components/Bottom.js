2d6630daf8313b7400aa7df4f2a698e6f86c4500
Manpreet Singh <ms20030904@gmail.com>
2024-07-12 08:18:23 +0530

    services

--- src/Components/Bottom.js
+++ src/Components/Bottom.js
@@ -7,18 +7,21 @@ const Bottom = () => {
   return (
     <div className="containerbottom">
         <div className="blueline"></div>
+        <div className="content">
        <div className="head">
         <span>What do we </span>
         <br />
         <span style={{ color: '#55B7CE' }}>Promise?</span>
        
        </div>
+       
        <div className="list">
             <ul>
                 <li> <img  style={{ height: '90px',width:'90px' }} src={time} alt="time" /> On time Delivery</li>
                 <li> <img  style={{ height: '90px',width:'90px' }} src={result} alt="result" /> Guranteed results</li>
                 <li>  <img  style={{ height: '90px',width:'90px' }} src={support} alt="support" /> 24*7 Support</li>
             </ul>
+        </div>
         </div>
     </div>
   )
